039bb47f307ccf3564b11f4a492c8db317c4779c
liting <luz.liting@gmail.com>
2023-02-01 15:18:52 +0800

    feat(components): add unocss

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   build: {
@@ -8,5 +9,5 @@ export default defineConfig({
       formats: ['es', 'cjs'],
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), UnoCSS()],
 })
